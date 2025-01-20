34df2063777265d3163676a4c35c5beaa94c83c9
a.iakunina <simonya6ka@gmail.com>
2025-01-20 10:27:23 +0300

    Expertise 1.3

--- js/skils-projects.js
+++ js/skils-projects.js
@@ -1,7 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
   const items = document.querySelectorAll('.skills__item');
-  console.log(items);
-  
   items.forEach(item => {
     const fillPercentage = item.getAttribute('data-fill');
     item.style.setProperty('--fill-percentage', `${fillPercentage}%`);
